982177724bab176977f205e3ef2f33961b8469db
shpokas270 <s.shpokas@gmail.com>
2024-09-10 15:18:35 +0300

    Added basic js functionality when pressed it takes to second page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Global scope  variables
+
+const form = document.querySelector(`form`);
+const card_1 = document.querySelector(`.card`);
+const card_2 = document.querySelector(`.card-2`);
+
+// -------------------------------------------------
+
+form.addEventListener(`submit`, (e) => {
+  e.preventDefault(); // Firstly adding action not to refresh the page after click
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
